01069f7801d84342dbf4f7497bb6810bfe935e3d
rpalloni <rpdev20@gmail.com>
2021-07-13 21:47:45 +0200

    heatmap

--- basic_viz.py
+++ basic_viz.py
@@ -60,3 +60,16 @@ ax.set_ylabel('ozone')
 ax.legend(facecolor='white')
 plt.show()
 
+
+# heatmap
+matrix = np.cov(ozone, solar).round(decimals=2)
+fig, ax = plt.subplots()
+ax.imshow(matrix)
+ax.grid(False)
+ax.xaxis.set(ticks=(0, 1), ticklabels=('x', 'y'))
+ax.yaxis.set(ticks=(0, 1), ticklabels=('x', 'y'))
+ax.set_ylim(1.5, -0.5)
+for i in range(2):
+    for j in range(2):
+        ax.text(j, i, matrix[i, j], ha='center', va='center', color='w')
+plt.show()

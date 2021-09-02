8efdedeeecbb295068a95fa88b42b2219b784eac
Roberto Palloni <rpdev20@gmail.com>
2021-09-02 15:25:30 +0200

    Update basic_mpl.py

--- basic_mpl.py
+++ basic_mpl.py
@@ -39,3 +39,68 @@ plt.scatter('a', 'b', c='c', s='d', data=data)
 plt.xlabel('entry a')
 plt.ylabel('entry b')
 plt.show()
+
+
+# categorical
+names = ['cats', 'dogs', 'dragons']
+values = [5, 25, 125]
+plt.figure(figsize=(15, 7))
+plt.subplot(131)
+plt.bar(names, values, color="red", label="bar chart")
+plt.legend()
+plt.subplot(132)
+plt.scatter(names, values, color="orange", label="scatter plot")
+plt.legend()
+plt.subplot(133)
+plt.plot(names, values, color="green", label="line plot")
+plt.legend()
+plt.suptitle('Categorical Plots')
+plt.show()
+
+# facets
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.2)
+
+plt.figure()
+
+plt.subplot(2,2,1)
+plt.plot(t1, f(t1), 'black')
+
+plt.subplot(2,2,2)
+plt.plot(t2, np.tan(2*np.pi*t2), 'r--')
+
+plt.subplot(2,2,3)
+plt.plot(t2, np.exp(t2), 'g^')
+
+plt.subplot(2,2,4)
+plt.plot(t2, np.cos(2*np.pi*t2), 'orange')
+
+# add text layer
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
+
+ax = plt.subplot()
+
+t = np.arange(0.0, 5.0, 0.01)
+s = np.cos(2*np.pi*t)
+line, = plt.plot(t, s, lw=2)
+
+plt.annotate('local maximum here', xy=(2, 1), xytext=(3, 1.5),
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             )
+
+plt.ylim(-2, 2)
+plt.show()
